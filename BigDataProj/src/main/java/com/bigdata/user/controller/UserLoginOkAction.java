09446c8ad91fdf6d70b1d255ec76593997224beb
kong-al <qaws1010@naver.com>
2022-11-09 22:52:02 +0900

    자동로그인기능 완성 (7일 유지)

--- BigDataProj/src/main/java/com/bigdata/user/controller/UserLoginOkAction.java
+++ BigDataProj/src/main/java/com/bigdata/user/controller/UserLoginOkAction.java
@@ -1,5 +1,8 @@
 package com.bigdata.user.controller;
 
+import java.net.HttpCookie;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,19 +21,27 @@ public class UserLoginOkAction implements Action {
 		UserVO uvo = new UserVO();
 		String user_name = req.getParameter("loginId");
 		String loginPw = req.getParameter("loginPw");
+		String autoLogin = req.getParameter("autoLogin");
 		
 		HttpSession session = req.getSession();
+		Cookie[] cookies = req.getCookies();
 		
 		if(udao.login(user_name,loginPw) != null) {
 			
 			uvo = udao.login(user_name,loginPw);
 			
 			if(uvo.getUser_pw().equals(loginPw)) {	
-				
 				session.setAttribute( "userInfo" , uvo );
-				
 				forward.setPath("/index.jsp");
 				forward.setRedirect(false);
+				
+				if(autoLogin.equals("true")) {
+					Cookie cookie = new Cookie("loginCookie", session.getId());
+					cookie.setPath("/");
+	                cookie.setMaxAge(60*60*24*7); 
+	                resp.addCookie(cookie);
+				}
+				
 			}else {
 				forward.setPath("/index.jsp");
 				forward.setRedirect(true);
